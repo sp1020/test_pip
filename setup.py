c99a6b5986eeef38acfdea7d63907f0bff74bebf
Seungpyo Hong <sp1020@gmail.com>
2021-05-06 15:48:33 +0900

    version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
  
 setup(name='test_pip', 
-	  version='0.0.1',
+	  version='0.0.2',
 	  description='Test for deployment of software with github using pip', 
 	  author='Seungpyo Hong',
 	  author_email='sp1020@gmail.com',
--- test_pip/__init__.py
+++ test_pip/__init__.py
@@ -1,3 +1,4 @@
 def test():
     print('software installed')
     print('a new version')
+    print('version update')
