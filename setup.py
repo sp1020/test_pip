9381e7688e8b50f6c02c0b8e78093da6ab1ac3d1
Seungpyo Hong <sp1020@gmail.com>
2021-05-06 15:30:26 +0900

    initial setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+  
+from setuptools import setup
+ 
+setup(name='test_pip', 
+	  version='0.0.1',
+	  description='Test for deployment of software with github using pip', 
+	  author='Seungpyo Hong',
+	  author_email='sp1020@gmail.com',
+	  url='',
+	  packages=['test_pip'], 
+	  include_package_data=True,
+  )
